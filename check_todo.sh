b6dba4b3fd6d47b029a3fb43bc16c22b36d6ae22
cubernetes <timo42@proton.me>
2024-08-14 04:05:09 +0200

    Add check_todo.sh script
    Also immediately add TODO to create GitHub Releases

---
+++ check_todo.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+# TODO: Create GitHub Releases
+
+# TODO: ONLY remove this file when the following command doesn't print anything anymore (except for this file)
+grep --color=always --exclude-dir=.git --exclude-dir=libft -inR -- '[T]ODO'
