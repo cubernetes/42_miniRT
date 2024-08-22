d2ee3bc4d3c1105e515b19b674018582af9dfb8a
tosu <timo42@proton.me>
2024-08-22 09:55:45 +0200

    Fix check_todo.sh script to check geometry folder from libft

--- check_todo.sh
+++ check_todo.sh
@@ -2,6 +2,21 @@
 
 # TODO: Create GitHub Releases
 # TODO: use sqrtf (float) instead of sqrt (double), benchmark it!
+# TODO: fix that mlx valgrind error regarding writing to uninitialized bytes
 
 # TODO: ONLY remove this file when the following command doesn't print anything anymore (except for this file)
-grep --color=always --exclude-dir=.git --exclude-dir=libft -inR -- '[T]ODO'
+grep --color=always \
+	--exclude-dir=.git/ \
+	--exclude-dir=data_constructors \
+	--exclude-dir=deque \
+	--exclude-dir=gc \
+	--exclude-dir=hashtable \
+	--exclude-dir=io \
+	--exclude-dir=list \
+	--exclude-dir=math \
+	--exclude-dir=memory \
+	--exclude-dir=misc \
+	--exclude-dir=string \
+	--exclude=ft_replace.h \
+	--exclude=libft.h \
+	-inR -- '[T]ODO'
