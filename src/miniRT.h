156a1a97a0b6814cf7028360ff5c4371b46defee
cubernetes <timo42@proton.me>
2024-09-04 16:06:34 +0200

    Norminette the miniRT header

--- src/miniRT.h
+++ src/miniRT.h
@@ -2,6 +2,7 @@
 # define MINIRT_H 1
 
 # include "libft.h"
+
 # include <stdlib.h>
 
 # define ANY_VEC 0
@@ -11,7 +12,7 @@
 
 /********** enums **********/
 
-enum					e_token_rt
+enum						e_token_rt
 {
 	TOK_AMB_LIGHT = 0,
 	TOK_CAMERA,
@@ -38,92 +39,92 @@ typedef enum e_direction	t_direction;
 typedef int					(*t_hook)(void *, ...);
 
 /* forward declarations */
-typedef struct s_rt_img	t_rt_img;
-typedef struct s_gc		t_gc;
-typedef struct s_scene	t_scene;
-typedef struct s_obj	t_obj;
-typedef struct s_light	t_light;
-typedef struct s_hit	t_hit;
-typedef struct s_camera	t_camera;
+typedef struct s_rt_img		t_rt_img;
+typedef struct s_gc			t_gc;
+typedef struct s_scene		t_scene;
+typedef struct s_obj		t_obj;
+typedef struct s_light		t_light;
+typedef struct s_hit		t_hit;
+typedef struct s_camera		t_camera;
 
 /********** struct defintions **********/
-struct					s_rt_img
+struct						s_rt_img
 {
-	void				*img;
-	char				*addr;
-	int					bpp;
-	int					line_length;
-	int					endian;
+	void					*img;
+	char					*addr;
+	int						bpp;
+	int						line_length;
+	int						endian;
 };
 
 /* gc == graphics context */
-struct					s_gc
+struct						s_gc
 {
-	void				*mlx;
-	void				*win;
-	t_rt_img			img;
-	t_scene				*scene;
+	void					*mlx;
+	void					*win;
+	t_rt_img				img;
+	t_scene					*scene;
 };
 
-struct					s_scene
+struct						s_scene
 {
-	int					wheight;
-	int					wwidth;
-	t_light				**lights;
-	t_obj				**objects;
-	int					nb_lights;
-	int					nb_objs;
-	t_vec3				camera_pos;
-	t_vec3				camera_dir;
-	double				fov;
+	int						wheight;
+	int						wwidth;
+	t_light					**lights;
+	t_obj					**objects;
+	int						nb_lights;
+	int						nb_objs;
+	t_vec3					camera_pos;
+	t_vec3					camera_dir;
+	double					fov;
 };
 
-struct					s_obj
+struct						s_obj
 {
-	t_token_rt			type;
-	t_color				color;
+	t_token_rt				type;
+	t_color					color;
 	union
 	{
-		t_plane			plane;
-		t_sphere		sphere;
-		t_cylinder		cylinder;
+		t_plane				plane;
+		t_sphere			sphere;
+		t_cylinder			cylinder;
 	};
 };
 
-struct					s_light
+struct						s_light
 {
-	t_vec3				*point;
-	t_color				color;
-	double				ratio;
+	t_vec3					*point;
+	t_color					color;
+	double					ratio;
 };
 
-struct					s_hit
+struct						s_hit
 {
-	double				t;
-	t_vec3				point;
-	t_vec3				norm;
-	t_vec3				ray_dir;
-	t_obj				*object;
-	t_color				color;
+	double					t;
+	t_vec3					point;
+	t_vec3					norm;
+	t_vec3					ray_dir;
+	t_obj					*object;
+	t_color					color;
 };
 
 /***************** prototypes ****************/
 /* main.c */
-void					finish(int exit_status, t_gc *gc);
+void						finish(int exit_status, t_gc *gc);
 
 /* mlx_helpers.c */
-void					mlx_pixel_put_buf(t_rt_img *data, int x, int y,
-							t_color color);
+void						mlx_pixel_put_buf(t_rt_img *data, int x, int y,
+								t_color color);
 // void	*mlx_new_resizable_window(t_xvar *xvar, int size_x, int size_y,
 // 			char *title);
 
 /* render.c */
-int						cast_ray(t_hit *hit, t_ray *ray, t_scene *scene);
-void					render(t_gc *gc, t_scene *scene);
-void					assert_norm(t_hit *hit, int index);
+int							cast_ray(t_hit *hit, t_ray *ray, t_scene *scene);
+void						render(t_gc *gc, t_scene *scene);
+void						assert_norm(t_hit *hit, int index);
 /* lights.c */
-void					apply_light(t_color *color, t_color light);
-t_color					calculate_lighting(t_hit *hit, t_scene *scene);
+void						apply_light(t_color *color, t_color light);
+t_color						calculate_lighting(t_hit *hit, t_scene *scene);
 
 /* parse.c */
 int							parse_input(t_obj ***objects, t_scene *scene);
