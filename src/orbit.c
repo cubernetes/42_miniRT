ba6e627bbeaa9a70f0d718dc175e4139eb9c8793
cubernetes <timo42@proton.me>
2024-09-19 13:05:58 +0200

    Float -> double in some places and remove forbidden function usleep

--- src/orbit.c
+++ src/orbit.c
@@ -6,10 +6,11 @@
 /*   By: tischmid <tischmid@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 10:56:01 by tischmid          #+#    #+#             */
-/*   Updated: 2024/09/19 10:56:27 by tischmid         ###   ########.fr       */
+/*   Updated: 2024/09/19 13:02:55 by tischmid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #define _XOPEN_SOURCE 600
 
 #include "miniRT.h"
@@ -19,8 +20,8 @@
 
 void	orbit(t_gc *gc, t_scene *scene)
 {
-	const float	angle = 3;
-	const float	amount = 2 * 200 * sinf(angle * 3 / 180);
+	const double	angle = 3;
+	const double	amount = 2 * 200 * sin(angle * 3 / 180);
 	int			i;
 
 	render(gc);
@@ -31,7 +32,7 @@ void	orbit(t_gc *gc, t_scene *scene)
 		rotate_camera(scene->camera, DIR_RIGHT, angle);
 		translate_camera(scene->camera, DIR_LEFT, amount);
 		rotate_camera(scene->camera, DIR_RIGHT, angle);
-		usleep(10000);
+		ft_sleep_linux(10);
 	}
 }
 		/* printf("%f,%f,%f\n", scene->camera->dir.x, scene->camera->dir.y, */
